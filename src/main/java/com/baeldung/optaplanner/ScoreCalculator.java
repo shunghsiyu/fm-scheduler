fc4598cf6f411aaa0249dd632a61e8957b3e538e
Shung-Hsi Yu <shunghsiyu@gmail.com>
2019-04-15 11:02:57 +0800

    Add ScoreCalculator class

--- src/main/java/com/baeldung/optaplanner/ScoreCalculator.java
+++ src/main/java/com/baeldung/optaplanner/ScoreCalculator.java
@@ -3,24 +3,46 @@ package com.baeldung.optaplanner;
 import org.optaplanner.core.api.score.Score;
 import org.optaplanner.core.api.score.buildin.hardsoft.HardSoftScore;
 import org.optaplanner.core.impl.score.director.easy.EasyScoreCalculator;
+import lombok.EqualsAndHashCode;
 
 import java.util.HashSet;
 
-public class ScoreCalculator implements EasyScoreCalculator<CourseSchedule> {
+public class ScoreCalculator implements EasyScoreCalculator<SchedulePlan> {
+
+    @EqualsAndHashCode
+    public class PersonAtTime {
+        private Person person;
+        private Time time;
+
+        public PersonAtTime(Person person, Time time) {
+            this.person = person;
+            this.time = time;
+        }
+
+        public Person getPerson() {
+            return this.person;
+        }
+
+        public Time getTime() {
+            return this.time;
+        }
+    }
 
     @Override
-    public Score calculateScore(CourseSchedule courseSchedule) {
+    public Score calculateScore(SchedulePlan plan) {
         int hardScore = 0;
         int softScore = 0;
 
-        HashSet<String> occupiedRooms = new HashSet<>();
-        for (Lecture lecture : courseSchedule.getLectureList()) {
-            if(lecture.getPeriod() != null && lecture.getRoomNumber() != null) {
-                String roomInUse = lecture.getPeriod().toString() + ":" + lecture.getRoomNumber().toString();
-                if (occupiedRooms.contains(roomInUse)) {
+        HashSet<PersonAtTime> occupancy = new HashSet<>();
+        for (Schedule sched : plan.getScheduleList()) {
+            Person person = sched.getAssignee();
+            Time time = sched.getTime();
+            if(person != null) {
+                PersonAtTime occupied = new PersonAtTime(person, time);
+                if (occupancy.contains(occupied)) {
                     hardScore += -1;
                 } else {
-                    occupiedRooms.add(roomInUse);
+                    occupancy.add(occupied);
                 }
             } else {
                 hardScore += -1;
--- src/test/java/com/baeldung/optaplanner/test/SchedulerUnitTest.java
+++ src/test/java/com/baeldung/optaplanner/test/SchedulerUnitTest.java
@@ -4,6 +4,7 @@ import com.baeldung.optaplanner.Person;
 import com.baeldung.optaplanner.Person.Gender;
 import com.baeldung.optaplanner.Time;
 import com.baeldung.optaplanner.Time.Period;
+import com.baeldung.optaplanner.ScoreCalculator;
 import com.baeldung.optaplanner.Schedule;
 import com.baeldung.optaplanner.Schedule.Type;
 import com.baeldung.optaplanner.SchedulePlan;
@@ -11,6 +12,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
+import org.optaplanner.core.api.score.buildin.hardsoft.HardSoftScore;
+import org.optaplanner.core.api.score.Score;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -165,5 +168,33 @@ public class SchedulerUnitTest {
 
         SchedulePlan plan = new SchedulePlan(persons, schedules);
         assertNotNull(plan);
+    }
+
+    @Test
+    public void test_calculateScore() {
+        List<Schedule> schedules = new ArrayList<>();
+        Schedule s1 = Schedule.PAP(new Time(1, 3, Period.MORNING));
+        schedules.add(s1);
+        Schedule s2 = Schedule.Jingfu(new Time(1, 2, Period.AFTERNOON));
+        schedules.add(s2);
+        Schedule s3 = Schedule.PAP(new Time(1, 1, Period.MORNING));
+        schedules.add(s3);
+        Schedule s4 = Schedule.PAP(new Time(1, 1, Period.MORNING));
+        schedules.add(s4);
+        
+        List<Person> persons = new ArrayList<>();
+        Person p1 = new Person("王小明", Gender.MALE, new Time(null, 4, Period.AFTERNOON));
+        persons.add(p1);
+        s3.setAssignee(p1);
+        s4.setAssignee(p1);
+        Person p2 = new Person("陳小美", Gender.FEMALE, new Time(null, 4, Period.MORNING));
+        persons.add(p2);
+
+        SchedulePlan plan = new SchedulePlan(persons, schedules);
+
+        ScoreCalculator calculator = new ScoreCalculator();
+        Score score = calculator.calculateScore(plan);
+        System.out.print(score.toString());
+        assertEquals(HardSoftScore.valueOf(-3, 0), score);
     }
 }
