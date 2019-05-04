9d779e47839e24869e88ab335279f7dfb12ccffa
Shung-Hsi Yu <shunghsiyu@gmail.com>
2019-05-05 01:09:15 +0800

    Add Person edit UI

--- src/Person.ts
+++ src/Person.ts
@@ -1,14 +1,21 @@
 export enum Gender {
-    Male,
-    Female
+  Male = "男",
+  Female = "女"
+}
+
+export enum Role {
+  Resident = "住院醫師",
+  AssistantChiefResident = "小 CR"
 }
 
 export default class Person {
-    name: string;
-    gender: Gender;
+  name: string;
+  gender: Gender;
+  role: Role;
 
-    constructor(name: string, gender: Gender) {
-        this.name = name;
-        this.gender = gender;
-    }
+  constructor(name: string, role: Role, gender: Gender) {
+    this.name = name;
+    this.role = role;
+    this.gender = gender;
+  }
 }
