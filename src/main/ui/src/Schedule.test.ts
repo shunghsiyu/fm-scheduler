86f0a68906a616e67e5a4c2039119a328e061f5c
Shung-Hsi Yu <shunghsiyu@gmail.com>
2019-05-07 19:19:50 +0800

    Add RepeatedSchedule class

---
+++ src/main/ui/src/Schedule.test.ts
@@ -0,0 +1,20 @@
+import Schedule, { RepeatedSchedule, Type } from './Schedule'
+import { DateTime } from "luxon";
+import Time, { Period, RepeatType } from "./Time";
+
+describe('RepeatedSchedule', () => {
+    test('Returns the correct schedules being repeated', () => {
+        const schedules = [
+            new Schedule(Type.PAP, new Time(DateTime.fromISO('2019-05-03'), Period.Afternoon)),
+            new Schedule(Type.PAP, new Time(DateTime.fromISO('2019-05-17'), Period.Afternoon)),
+            new Schedule(Type.PAP, new Time(DateTime.fromISO('2019-05-31'), Period.Afternoon)),
+        ];
+        const repeatedSchedule = new RepeatedSchedule(Type.PAP, 2019, 5, {
+            type: RepeatType.OddWeek,
+            weekday: 5,
+            period: Period.Afternoon
+        });
+        expect(repeatedSchedule.toSchedules()).toEqual(schedules);
+    });
+
+});
