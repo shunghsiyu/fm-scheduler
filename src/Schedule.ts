07911e8c68447b8213640650a29bebe0771041ac
Shung-Hsi Yu <shunghsiyu@gmail.com>
2019-05-07 19:19:50 +0800

    Add RepeatedSchedule class

--- src/Schedule.ts
+++ src/Schedule.ts
@@ -1,4 +1,4 @@
-import Time from "./Time";
+import Time, { getTimes, Repeat } from "./Time";
 import Person from "./Person";
 
 export enum Type {
@@ -15,11 +15,32 @@ export enum Type {
 export default class Schedule {
     type: Type;
     time: Time;
-    assignee: Person;
+    assignee?: Person;
 
-    constructor(type: Type, time: Time, assignee: Person) {
+    constructor(type: Type, time: Time, assignee?: Person) {
         this.type = type;
         this.time = time;
         this.assignee = assignee;
+    }
+}
+
+export class RepeatedSchedule {
+    type: Type;
+    year: number;
+    month: number;
+    repeat: Repeat;
+    assignee?: Person;
+
+    constructor(type: Type, year: number, month: number, repeat: Repeat, assignee?: Person) {
+        this.type = type;
+        this.year = year;
+        this.month = month;
+        this.repeat = repeat;
+        this.assignee = assignee;
+    }
+
+    toSchedules(): Array<Schedule> {
+        return getTimes(this.year, this.month, this.repeat)
+            .map(time => new Schedule(this.type, time, this.assignee))
     }
 }
